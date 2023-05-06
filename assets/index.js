0e5d02fe68a801f8125df17338308806355ff18b
Luiz Mendes <luiz.mendes@in8.com.br>
2023-05-06 09:18:55 -0300

    style: add color

--- assets/index.js
+++ assets/index.js
@@ -2,22 +2,38 @@ const url = 'http://54.244.79.39:3000/product'
 
 
 const getProducts = async () => {
-    const result = await fetch(url)
+  const result = await fetch(url)
 
-    const products = await result.json()
-    let productsHtml = ''
-    for(const product of products){
-        productsHtml += `<div class="card" style="width: 18rem;">
-        <img src="${product.image}" class="card-img-top" alt="...">
-        <div class="card-body">
-          <h5 class="card-title">${product.description}</h5>
-          <p class="card-text">R$ ${product.price.toFixed(2)}</p>
-          <a href="https://www.kabum.com.br/busca/${product.description}" class="btn btn-primary">Ir para o site</a>
+  const products = await result.json()
+  let productsHtml = ''
+  for (const product of products) {
+    productsHtml += `
+        <div class='col-6 d-flex justify-content-center'>
+        <div class="card mb-3" style="max-width: 540px; min-height: 200px">
+          <div class="row g-0">
+            <div class="col-md-4">
+              <img src="${product.image}" class="img-fluid rounded-start m-2" alt="...">
+            </div>
+            <div class="col-md-8">
+              <div class="card-body">
+                <h5 class="card-title">${product.description}</h5>
+              </div>
+
+              <div class="card-body">
+                <h5 class="card-title" style="color:#FF0000">R$ ${product.price.toFixed(2).replace(',','').replace('.',',')}</h5>
+              </div>
+            </div>
+
+            <div class="card-footer text-end">
+            <a href="https://www.kabum.com.br/busca/${product.description}" class="btn btn-primary">Verificar no site</a>
+          </div>
+          </div>
         </div>
-      </div>`
-    }
+      </div>
+      `
+  }
 
-    document.querySelector('#products').innerHTML = `<div class='row'>${productsHtml}</div>`
+  document.querySelector('#products').innerHTML = `<div class='row'>${productsHtml}</div>`
 
 }
 
