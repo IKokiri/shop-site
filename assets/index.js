8968ea2c10bae51e9aa6abfda6c16787cca29c4a
Luiz Mendes <luiz.mendes@in8.com.br>
2023-05-07 12:50:47 -0300

    sort

--- assets/index.js
+++ assets/index.js
@@ -1,16 +1,32 @@
 const url = 'http://54.244.79.39:3000/product'
-
-
+let checkedTags = []
 const getProducts = async () => {
+  const listTags = []
   const result = await fetch(url)
 
   const products = await result.json()
   let productsHtml = ''
   for (const product of products) {
+
+    let hasTag = true
+    if(checkedTags.length > 0){
+      for(let tag of checkedTags){
+        if(!product.description.toUpperCase().includes(tag.toUpperCase())) hasTag = false
+      }
+    }
+
+    if(!hasTag) continue
     let link = ''
 
-    if(product.description.includes('K001')) link = `https://www.kabum.com.br/busca/${product.description.replace(' - K001','')}`
-    if(product.description.includes('P001')) link = `https://www.pichau.com.br/${product.description.replace(' - P001','').replaceAll(' ','-').replaceAll(',','').replaceAll('.','-').replaceAll('/','-').replaceAll('--','-')}`
+    if (product.description.includes('K001')) link = `https://www.kabum.com.br/busca/${product.description.replace(' - K001', '')}`
+    if (product.description.includes('P001')) link = `https://www.pichau.com.br/${product.description.replace(' - P001', '').replaceAll(' ', '-').replaceAll(',', '').replaceAll('.', '-').replaceAll('/', '-').replaceAll('--', '-')}`
+
+    const currentTags = product.description.split(' ')
+
+    for (let currentTag of currentTags) {
+      if (listTags.includes(currentTag.replace(',','').toUpperCase())) continue
+      listTags.push(currentTag.replace(',','').toUpperCase())
+    }
 
     productsHtml += `
         <div class='col-md-6 col-sm-12 d-flex justify-content-center'>
@@ -25,7 +41,7 @@ const getProducts = async () => {
               </div>
 
               <div class="card-body">
-                <h5 class="card-title" style="color:#FF0000">R$ ${product.price.toFixed(2).replace(',','').replace('.',',')}</h5>
+                <h5 class="card-title" style="color:#FF0000">R$ ${product.price.toFixed(2).replace(',', '').replace('.', ',')}</h5>
               </div>
             </div>
 
@@ -38,7 +54,42 @@ const getProducts = async () => {
       `
   }
 
+  showTags(listTags)
   document.querySelector('#products').innerHTML = `<div class='row'>${productsHtml}</div>`
+}
+
+const showTags = (listTags) => {
+  listTags = listTags.sort()
+  let htmlTags = ''
+  for (const tag of listTags) {
+    let checked = ''
+    
+    if(checkedTags.includes(tag)) checked = 'checked'
+    htmlTags += `
+    <div class='col-2'>
+    <div class="form-check">
+    <input class="form-check-input chkTags" type="checkbox" value="${tag.replace(',','')}" ${checked} id="flexCheckIndeterminate">
+    <label class="form-check-label" for="flexCheckIndeterminate">
+      ${tag.replace(',','')}
+    </label>
+    </div>
+    </div>
+  `
+  }
+
+  document.querySelector('#tagProducts').innerHTML = `<div class='row'>${htmlTags}</div>`
+
+  const checkboxes = document.querySelectorAll('.chkTags');
+
+  checkboxes.forEach((checkbox) => {
+    checkbox.addEventListener('change', () => {
+      const checkboxes = document.querySelectorAll('input[type="checkbox"]:checked');
+
+      checkedTags = Array.from(checkboxes).map((checkbox) => checkbox.value);
+
+      getProducts()
+    });
+  });
 
 }
 
